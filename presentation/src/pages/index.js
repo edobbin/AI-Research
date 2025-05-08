d76e311e6ffb6dd04adbeff30dbfe4ab19e58c8b
danoruo1 <danielanoruo928@gmail.com>
2025-05-08 13:22:50 -0400

    Visual Presentation

--- presentation/src/pages/index.js
+++ presentation/src/pages/index.js
@@ -1,5 +1,7 @@
 import Image from "next/image";
+import {Typography} from "@mui/material";
 import { Geist, Geist_Mono } from "next/font/google";
+import TypewriterComponent from "typewriter-effect";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -11,64 +13,53 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
+const titles = [''];
+
 export default function Home() {
   return (
     <div
-      className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/pages/index.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+      className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`} style={{background:"transparent"}}
+   >
+        <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          objectFit: 'cover',
+          zIndex: -1
+        }}
+      >
+        <source src="/videos/presentation.mp4" type="video/mp4" />       
+         Your browser does not support the video tag.
+      </video>
+      
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start" 
+          style={{sfontFamily: "Space Grotesk",
+          position: "sticky",
+          fontSize: "5vw",
+          transform: "scale(1)"}}>
+
+            
+      <TypewriterComponent options={{strings: ['The Broken Windows Theory'], autoStart:true,cursor:"_",loop:true}} sx={{fontFamily:"Space Grotesk",
+          position:"sticky",
+          scale:"auto",
+          fontSize:'clamp(1.5rem, 5vw, 4rem)'}}/>
+
+        
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+          href="https://github.com/edobbin/AI-Research"
           target="_blank"
           rel="noopener noreferrer"
+          fontFamily="Space Grotesk"
         >
           <Image
             aria-hidden
@@ -77,11 +68,11 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Learn
+          Github
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+          href="https://www.britannica.com/topic/broken-windows-theory"
           target="_blank"
           rel="noopener noreferrer"
         >
@@ -91,14 +82,16 @@ export default function Home() {
             alt="Window icon"
             width={16}
             height={16}
+            fontFamily="Space Grotesk"
           />
-          Examples
+          Reference
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+          href="https://www.linkedin.com/search/results/all/?keywords=Baltimore%20Data%20Science%20Corps&sid=R7d"
           target="_blank"
           rel="noopener noreferrer"
+          fontFamily="Space Grotesk"
         >
           <Image
             aria-hidden
@@ -107,7 +100,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
+          LinkedIn
         </a>
       </footer>
     </div>
