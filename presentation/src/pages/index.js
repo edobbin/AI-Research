1f68401e88125ebf3b4c3e5f07eb5c54ada004a1
danoruo1 <danielanoruo928@gmail.com>
2025-05-08 14:30:17 -0400

    Visual Edits #2

--- presentation/src/pages/index.js
+++ presentation/src/pages/index.js
@@ -1,7 +1,9 @@
 import Image from "next/image";
-import {Typography} from "@mui/material";
+import {Typography,Button, Container} from "@mui/material";
 import { Geist, Geist_Mono } from "next/font/google";
 import TypewriterComponent from "typewriter-effect";
+import { useEffect, useState } from "react";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -13,9 +15,10 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-const titles = [''];
+const titles = ['Title','Introduction','Factor(s) Observed','Demographics','Linear Regression Model','Integration with Neural Network','Adjustments', 'Results'];
 
 export default function Home() {
+  const [title,setTitle] = useState(0);
   return (
     <div
       className={`${geistSans.className} ${geistMono.className} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`} style={{background:"transparent"}}
@@ -32,28 +35,85 @@ export default function Home() {
           width: '100%',
           height: '100%',
           objectFit: 'cover',
-          zIndex: -1
+          zIndex: -1,
+          opacity:.5
         }}
       >
         <source src="/videos/presentation.mp4" type="video/mp4" />       
          Your browser does not support the video tag.
       </video>
       
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start" 
+      <main 
           style={{sfontFamily: "Space Grotesk",
           position: "sticky",
           fontSize: "5vw",
-          transform: "scale(1)"}}>
-
+          transform: "scale(1)",
+          top:"120px"
+          }}>
             
-      <TypewriterComponent options={{strings: ['The Broken Windows Theory'], autoStart:true,cursor:"_",loop:true}} sx={{fontFamily:"Space Grotesk",
-          position:"sticky",
-          scale:"auto",
-          fontSize:'clamp(1.5rem, 5vw, 4rem)'}}/>
-
-        
+            <TypewriterComponent
+              options={{
+              autoStart: true,
+              loop: false,
+              cursor: "_",
+            }}
+            onInit={(typewriter) => {
+              typewriter
+                .typeString('The Broken Windows Theory')
+                .callFunction(() => {
+                  document.querySelector('.Typewriter__cursor')?.classList.add('hidden');
+                })
+                .start();
+            }}
+          />
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
+
+      <div
+          style={{
+            width: "24vw",
+            height: "5vh",
+            display: "flex",
+            gap: "2rem", // or adjust as needed
+            justifyContent: "center",
+            alignItems: "center",
+            position: "relative",
+            top:"-150px"            
+          }}
+        >
+            <TypewriterComponent sx={{fontFamily:"Space Grotesk",
+            position:"sticky",
+            scale:"auto",
+            fontSize:'clamp(2.5rem, 3vw, 4.5rem)',
+            }}
+             options={{ autoStart:true,cursor:"_"}} 
+            onInit={(typewriter) => {
+              typewriter
+                .typeString('By: Daniel Anoruo and Ethan Dobbin')
+                .callFunction(() => {
+                  document.querySelector('.Typewriter__cursor')?.classList.add('hidden');
+                })
+                .start();
+                
+            }}/>
+
+        </div>
+      <div
+          style={{
+            width: "50vw",
+            height: "10vh",
+            display: "flex",
+            gap: "5rem", // or adjust as needed
+            justifyContent: "center",
+            alignItems: "center",
+            position: "sticky",
+          }}
+        >
+          <Button sx={{width:"10vw",height:"7vh",background:"gray", border:"3px solid black", fontFamily:"Space Grotesk", color:"black",fontSize:"medium"}}>Prev</Button>
+          <Button sx={{width:"10vw",height:"7vh",background:"gray", border:"3px solid black", fontFamily:"Space Grotesk", color:"black",fontSize:"medium"}}>Next</Button>
+
+      </div>
+
+      <footer className="flex gap-[24px] flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://github.com/edobbin/AI-Research"
